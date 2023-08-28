0c01274582f4cd547e84984e246fa470ec131616
luwam <lwamyohannes2018@gmail.com>
2023-08-28 17:10:18 -0600

    low_average_completion_time.sql added

---
+++ 3_queries_group-by/low_average_completion_time.sql
@@ -0,0 +1,11 @@
+SELECT students.name AS student, AVG(assignment_submissions.duration) AS average_assignment_duration, AVG(assignments.duration) As average_estimated_duration 
+FROM students
+JOIN assignment_submissions ON students.id = student_id
+JOIN assignments ON assignments.id = student_id
+
+WHERE end_date IS NOT NULL 
+
+GROUP BY student
+HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
+ORDER BY average_assignment_duration;
+
